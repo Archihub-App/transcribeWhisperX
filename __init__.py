e83fc9e0965f42ea52a4e0d9864b9484ccda507c
Nestor Pena <nestor2509@gmail.com>
2025-03-10 16:46:27 -0500

    avances

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from app.utils.PluginClass import PluginClass
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import request
+from flask import request, send_file
 from app.utils import DatabaseHandler
 from app.api.records.models import RecordUpdate
 from celery import shared_task
 import os
+import shutil
 from bson.objectid import ObjectId
 from app.utils.LogActions import log_actions
 from app.api.logs.services import register_log
@@ -15,7 +16,9 @@ load_dotenv()
 
 mongodb = DatabaseHandler.DatabaseHandler()
 WEB_FILES_PATH = os.environ.get('WEB_FILES_PATH', '')
+USER_FILES_PATH = os.environ.get('USER_FILES_PATH', '')
 ORIGINAL_FILES_PATH = os.environ.get('ORIGINAL_FILES_PATH', '')
+TEMPORAL_FILES_PATH = os.environ.get('TEMPORAL_FILES_PATH', '')
 HF_TOKEN = os.environ.get('HF_TOKEN', '')
 batch_size = 16
 compute_type='float32'
@@ -44,26 +47,123 @@ class ExtendedPluginClass(PluginClass):
         def download():
             current_user = get_jwt_identity()
             body = request.get_json()
-
-            self.validate_fields(body, 'download')
+            
+            print(body)
+            
             self.validate_roles(current_user, ['admin', 'processing', 'editor'])
 
-            record = mongodb.get_record('records', {'_id': ObjectId(body['id'])})
-            if not record:
-                return {'msg': 'No se encontró el recurso'}, 404
+            task = self.download.delay(body, current_user)
+            self.add_task_to_user(task.id, 'transcribeWhisperX.download', current_user, 'file_download')
             
-            if 'transcribeWhisperX' in record['processing']:
-                result = record['processing']['transcribeWhisperX']['result']
-                if 'text' in result:
-                    text = result['text']
-                else:
-                    text = result['segments']
-                return {'text': text}, 200
-            else:
-                return {'msg': 'No se encontró la transcripción'}, 404
+            return {'msg': 'Se agregó la tarea a la fila de procesamientos'}, 201
         
-    def indexing(self, record):
-        pass
+        @self.route('/filedownload/<taskId>', methods=['GET'])
+        @jwt_required()
+        def file_download(taskId):
+            current_user = get_jwt_identity()
+
+            if not self.has_role('admin', current_user) and not self.has_role('processing', current_user) and not self.has_role('editor', current_user):
+                return {'msg': 'No tiene permisos suficientes'}, 401
+            
+            # Buscar la tarea en la base de datos
+            task = mongodb.get_record('tasks', {'taskId': taskId})
+            # Si la tarea no existe, retornar error
+            if not task:
+                return {'msg': 'Tarea no existe'}, 404
+            
+            if task['user'] != current_user and not self.has_role('admin', current_user):
+                return {'msg': 'No tiene permisos para obtener la tarea'}, 401
+
+            if task['status'] == 'pending':
+                return {'msg': 'Tarea en proceso'}, 400
+
+            if task['status'] == 'failed':
+                return {'msg': 'Tarea fallida'}, 400
+
+            if task['status'] == 'completed':
+                if task['resultType'] != 'file_download':
+                    return {'msg': 'Tarea no es de tipo file_download'}, 400
+                
+            path = USER_FILES_PATH + task['result']
+            response = send_file(path, as_attachment=True, download_name=os.path.basename(path), conditional=False)
+            
+            response.headers.add("Access-Control-Expose-Headers", "Content-Disposition")
+            return response
+        
+        
+    @shared_task(ignore_result=False, name='transcribeWhisperX.download')
+    def download(body, user):
+        records_filters = {'_id': {'$in': [ObjectId(record) for record in body['records']]}}
+        
+        records = list(mongodb.get_all_records('records', records_filters, fields={'_id': 1, 'mime': 1, 'filepath': 1, 'processing': 1, 'name': 1, 'displayName': 1}))
+        
+        if len(records) == 0:
+            raise Exception('No se encontraron registros')
+        elif len(records) > 1:
+            raise Exception('Debe seleccionar solo un registro')
+        
+        folder_path = USER_FILES_PATH + '/' + user + '/transcribeWhisperX'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        
+        for r in records:
+            if 'transcribeWhisperX' in r['processing']:
+                result = r['processing']['transcribeWhisperX']['result']
+
+                if body['format'] == 'doc':
+                    from docx import Document
+                    doc = Document()
+                    title = r['displayName'] if 'displayName' in r else r['name']
+                    doc.add_heading(title, 0)
+                    doc.add_paragraph(result['text'])
+                    path = os.path.join(USER_FILES_PATH, user, 'transcribeWhisperX', str(r['_id']) + '.docx')
+                    doc.save(path)
+                    return '/' + user + '/transcribeWhisperX/' + str(r['_id']) + '.docx'
+                elif body['format'] == 'pdf':
+                    from docx import Document
+                    doc = Document()
+                    title = r['displayName'] if 'displayName' in r else r['name']
+                    doc.add_heading(title, 0)
+                    doc.add_paragraph(result['text'])
+                    temp_path = os.path.join(TEMPORAL_FILES_PATH, str(r['_id']) + '.docx')
+                    doc.save(temp_path)
+                    
+                    try:
+                        from app.plugins.filesProcessing.utils.DocumentProcessing import convert_to_pdf_with_libreoffice
+                    except Exception as e:
+                        raise Exception('Error al importar el módulo del plugin para el procesamiento de documentos: ' + str(e))
+                    
+                    output_pdf = os.path.join(USER_FILES_PATH, user, 'transcribeWhisperX', str(r['_id']) + '.pdf')
+                    convert_to_pdf_with_libreoffice(temp_path, output_pdf)
+                    shutil.move(os.path.join(TEMPORAL_FILES_PATH, str(r['_id']) + '.pdf'), output_pdf)
+                    os.remove(temp_path)
+                    return '/' + user + '/transcribeWhisperX/' + str(r['_id']) + '.pdf'
+                elif body['format'] == 'srt':
+                    def millis_to_srt_time(millis):
+                        # Convert millis to an integer
+                        millis = int(millis)
+                        seconds, ms = divmod(millis, 1000)
+                        minutes, sec = divmod(seconds, 60)
+                        hours, minutes = divmod(minutes, 60)
+                        return f"{hours:02d}:{minutes:02d}:{sec:02d},{ms:03d}"
+                    
+                    segments = result['segments']
+                    srt = ''
+                    for i, segment in enumerate(segments):
+                        start_str = millis_to_srt_time(segment['start'])
+                        end_str = millis_to_srt_time(segment['end'])
+                        srt += str(i + 1) + '\n'
+                        srt += start_str + ' --> ' + end_str + '\n'
+                        srt += segment['text'] + '\n\n'
+                    path = os.path.join(USER_FILES_PATH, user, 'transcribeWhisperX', str(r['_id']) + '.srt')
+                    with open(path, 'w') as f:
+                        f.write(srt)
+                    return '/' + user + '/transcribeWhisperX/' + str(r['_id']) + '.srt'
+                else:
+                    raise Exception('Formato no válido')
+                    
+                    
+                
         
     @shared_task(ignore_result=False, name='transcribeWhisperX.bulk', queue='high')
     def bulk(body, user):
@@ -71,35 +171,38 @@ class ExtendedPluginClass(PluginClass):
         id_process = []
 
         import torch
-        if body['gpu']:
+        if 'gpu' in body and body['gpu']:
             device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         else:
             device = torch.device('cpu')
 
-        filters = {
-            'post_type': body['post_type']
-        }
+        if 'records' not in body:
+            filters = {
+                'post_type': body['post_type']
+            }
 
-        if 'parent' in body:
-            if body['parent'] and len(body['resources']) == 0:
-                filters = {'$or': [{'parents.id': body['parent'], 'post_type': body['post_type']}, {'_id': ObjectId(body['parent'])}], **filters}
-        
-        if 'resources' in body:
-            if body['resources']:
-                if len(body['resources']) > 0:
-                    filters = {'_id': {'$in': [ObjectId(resource) for resource in body['resources']]}, **filters}
+            if 'parent' in body:
+                if body['parent'] and len(body['resources']) == 0:
+                    filters = {'$or': [{'parents.id': body['parent'], 'post_type': body['post_type']}, {'_id': ObjectId(body['parent'])}], **filters}
             
-        # obtenemos los recursos
-        resources = list(mongodb.get_all_records('resources', filters, fields={'_id': 1}))
-        resources = [str(resource['_id']) for resource in resources]
+            if 'resources' in body:
+                if body['resources']:
+                    if len(body['resources']) > 0:
+                        filters = {'_id': {'$in': [ObjectId(resource) for resource in body['resources']]}, **filters}
+                
+            # obtenemos los recursos
+            resources = list(mongodb.get_all_records('resources', filters, fields={'_id': 1}))
+            resources = [str(resource['_id']) for resource in resources]
 
-        records_filters = {
-            'parent.id': {'$in': resources},
-            'processing.fileProcessing': {'$exists': True},
-            '$or':[{'processing.fileProcessing.type': 'audio'}, {'processing.fileProcessing.type': 'video'}]
-        }
+            records_filters = {
+                'parent.id': {'$in': resources},
+                'processing.fileProcessing': {'$exists': True},
+                '$or':[{'processing.fileProcessing.type': 'audio'}, {'processing.fileProcessing.type': 'video'}]
+            }
+        else:
+            records_filters = {'_id': {'$in': [ObjectId(record) for record in body['records']]}}
 
-        if body['overwrite']:
+        if 'overwrite' in body and body['overwrite']:
             records_filters = {"$or": [{"processing.fileProcessing": {"$exists": False}, **records_filters}, {"processing.fileProcessing": {"$exists": True}, **records_filters}]}
         else:
             records_filters['processing.transcribeWhisperX'] = {'$exists': False}
@@ -112,8 +215,6 @@ class ExtendedPluginClass(PluginClass):
             if body['diarize']:
                 import whisperx
                 diarize_model = whisperx.DiarizationPipeline(use_auth_token=HF_TOKEN, device=device)
-                # from pyannote.audio import Pipeline
-                # diarize_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1",use_auth_token=HF_TOKEN)
 
         for r in records:
             file_path = os.path.join(ORIGINAL_FILES_PATH, r['filepath'])
@@ -178,6 +279,60 @@ class ExtendedPluginClass(PluginClass):
         instance.clear_cache()
         return 'Transcripción automática finalizada'
         
+general_settings = [
+    {
+        'type': 'checkbox',
+        'label': 'Sobreescribir procesamientos existentes',
+        'id': 'overwrite',
+        'default': False,
+        'required': False,
+    },
+    {
+        'type': 'checkbox',
+        'label': 'Separar parlantes',
+        'id': 'diarize',
+        'default': False,
+        'required': False,
+    },
+    {
+        'type': 'checkbox',
+        'label': 'Usar GPU (si está disponible)',
+        'id': 'gpu',
+        'default': False,
+        'required': False,
+    },
+    {
+        'type': 'select',
+        'label': 'Tamaño del modelo',
+        'id': 'model',
+        'default': 'turbo',
+        'options': [
+            {'value': 'tiny', 'label': 'Muy pequeño'},
+            {'value': 'small', 'label': 'Pequeño'},
+            {'value': 'medium', 'label': 'Mediano'},
+            {'value': 'large', 'label': 'Grande'},
+            {'value': 'turbo', 'label': 'Turbo'},
+        ],
+        'required': False,
+    },
+    {
+        'type': 'select',
+        'label': 'Idioma de la transcripción',
+        'id': 'language',
+        'default': 'auto',
+        'options': [
+            {'value': 'auto', 'label': 'Automático'},
+            {'value': 'es', 'label': 'Español'},
+            {'value': 'en', 'label': 'Inglés'},
+            {'value': 'fr', 'label': 'Francés'},
+            {'value': 'de', 'label': 'Alemán'},
+            {'value': 'it', 'label': 'Italiano'},
+            {'value': 'pt', 'label': 'Portugués'},
+            
+        ],
+        'required': False,
+    }
+]
     
 plugin_info = {
     'name': 'Transcripción automática',
@@ -192,58 +347,7 @@ plugin_info = {
                 'title': 'Instrucciones',
                 'text': 'Este plugin permite la transcripción automática de audios y videos usando el modelo WhisperX. Para ello, debe seleccionar el tipo de contenido y el recurso padre. El plugin procesará todos los archivos de audio y video de los recursos hijos del recurso padre seleccionado. Si el recurso padre no está seleccionado, el plugin procesará todos los archivos de audio y video de todos los recursos del tipo de contenido seleccionado.',
             },
-            {
-                'type': 'checkbox',
-                'label': 'Sobreescribir procesamientos existentes',
-                'id': 'overwrite',
-                'default': False,
-                'required': False,
-            },
-            {
-                'type': 'checkbox',
-                'label': 'Separar parlantes',
-                'id': 'diarize',
-                'default': False,
-                'required': False,
-            },
-            {
-                'type': 'checkbox',
-                'label': 'Usar GPU (si está disponible)',
-                'id': 'gpu',
-                'default': False,
-                'required': False,
-            },
-            {
-                'type': 'select',
-                'label': 'Tamaño del modelo',
-                'id': 'model',
-                'default': 'small',
-                'options': [
-                    {'value': 'tiny', 'label': 'Muy pequeño'},
-                    {'value': 'small', 'label': 'Pequeño'},
-                    {'value': 'medium', 'label': 'Mediano'},
-                    {'value': 'large', 'label': 'Grande'},
-                    {'value': 'turbo', 'label': 'Turbo'},
-                ],
-                'required': False,
-            },
-            {
-                'type': 'select',
-                'label': 'Idioma de la transcripción',
-                'id': 'language',
-                'default': 'auto',
-                'options': [
-                    {'value': 'auto', 'label': 'Automático'},
-                    {'value': 'es', 'label': 'Español'},
-                    {'value': 'en', 'label': 'Inglés'},
-                    {'value': 'fr', 'label': 'Francés'},
-                    {'value': 'de', 'label': 'Alemán'},
-                    {'value': 'it', 'label': 'Italiano'},
-                    {'value': 'pt', 'label': 'Portugués'},
-                    
-                ],
-                'required': False,
-            }
+            *general_settings
         ]
     },
     'actions': [
@@ -255,51 +359,7 @@ plugin_info = {
             'endpoint': 'bulk',
             'icon': 'Transcribe',
             'extraOpts': [
-                {
-                    'type': 'checkbox',
-                    'label': 'Separar parlantes',
-                    'id': 'diarize',
-                    'default': False,
-                    'required': False,
-                },
-                {
-                    'type': 'checkbox',
-                    'label': 'Usar GPU (si está disponible)',
-                    'id': 'gpu',
-                    'default': False,
-                    'required': False,
-                },
-                {
-                    'type': 'select',
-                    'label': 'Tamaño del modelo',
-                    'id': 'model',
-                    'default': 'small',
-                    'options': [
-                        {'value': 'tiny', 'label': 'Muy pequeño'},
-                        {'value': 'small', 'label': 'Pequeño'},
-                        {'value': 'medium', 'label': 'Mediano'},
-                        {'value': 'large', 'label': 'Grande'},
-                        {'value': 'turbo', 'label': 'Turbo'},
-                    ],
-                    'required': False,
-                },
-                {
-                    'type': 'select',
-                    'label': 'Idioma de la transcripción',
-                    'id': 'language',
-                    'default': 'auto',
-                    'options': [
-                        {'value': 'auto', 'label': 'Automático'},
-                        {'value': 'es', 'label': 'Español'},
-                        {'value': 'en', 'label': 'Inglés'},
-                        {'value': 'fr', 'label': 'Francés'},
-                        {'value': 'de', 'label': 'Alemán'},
-                        {'value': 'it', 'label': 'Italiano'},
-                        {'value': 'pt', 'label': 'Portugués'},
-                        
-                    ],
-                    'required': False,
-                }
+                *general_settings
             ]
         },
         {
@@ -313,8 +373,8 @@ plugin_info = {
                 {
                     'type': 'select',
                     'label': 'Formato del archivo',
-                    'id': 'language',
-                    'default': 'auto',
+                    'id': 'format',
+                    'default': 'pdf',
                     'options': [
                         {'value': 'pdf', 'label': 'PDF'},
                         {'value': 'doc', 'label': 'DOC'},
